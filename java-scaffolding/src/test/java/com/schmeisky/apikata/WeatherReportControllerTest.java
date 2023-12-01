178b2695f2d67565eb3d929af2df2d091245414d
johan martinsson <johan.martinsson@etat.ge.ch>
2023-12-01 10:46:20 +0100

    remove scaffolding tests

--- java-scaffolding/src/test/java/com/schmeisky/apikata/WeatherReportControllerTest.java
+++ java-scaffolding/src/test/java/com/schmeisky/apikata/WeatherReportControllerTest.java
@@ -44,23 +44,4 @@ class WeatherReportControllerTest {
         Assertions.assertThat(content.get(1)).matches("1,Reykjavik,.*,.*,.*,.*,.*");
     }
 
-    @Test
-    void sandbox() {
-
-        var text = "{\"results\":[{\"name\":\"Reykjavík\",\"time\":\"2023-11-30 21:00:00\",\"err\":\"\",\"link\":\"http://en.vedur.is/weather/observations/areas/reykjavik/#group=100&station=1\",\"F\":\"6\",\"FX\":\"8\",\"FG\":\"13\",\"D\":\"NNE\",\"T\":\"-0.8\",\"W\":\"Light snow\",\"V\":\"12\",\"N\":\"100\",\"P\":\"1029\",\"RH\":\"80\",\"SNC\":\"\",\"SND\":\"\",\"SED\":\"\",\"RTE\":\"\",\"TD\":\"-3.8\",\"R\":\"0.1\",\"id\":\"1\",\"valid\":\"1\"}]}";
-        final Gson gson = new Gson();
-        final WeatherReportController.Result result = gson.fromJson(text, WeatherReportController.Result.class);
-    }
-
-    @ParameterizedTest
-    @CsvSource(textBlock =
-//            "1,Reykjavík,2023-11-30,21:00:00,-0.8,1029,NNE 1,Reykjavik,.*,.*,.*,.*,.\n" +
-//            "1,Reykjavík,2023-11-30,21:00:00 1,Reykjavik,.*,.*\n" +
-//            "1,Reykjavík 1,Reykjavik\n" +
-            "1,Reykjavík 1,Reykjavík\n" +
-            "1,1 1,1"
-            , delimiter = ' ')
-    void sbox2(String str, String pattern) {
-        Assertions.assertThat(str).matches(pattern);
-    }
 }
